b7847455b71dbeb009f18ad14d20cddf0a5b87a6
skevet-avramuk <skeavravashvetmukchuk@gmail.com>
2025-12-25 19:56:26 +0300

    rewrite users.controller file

--- students/VashchukAnatoliy/test_02/backend/api/src/users/users.controller.ts
+++ students/VashchukAnatoliy/test_02/backend/api/src/users/users.controller.ts
@@ -1,15 +1,18 @@
-@Get(':id/followers')
-getFollowers(
-  @Param('id') userId: string,
-  @Query() query: FollowersQueryDto,
-) {
-  return this.usersService.getFollowers(userId, query.cursor, query.limit);
-}
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { FollowersQueryDto } from './dto/followers-query.dto';
 
-@Get(':id/following')
-getFollowing(
-  @Param('id') userId: string,
-  @Query() query: FollowersQueryDto,
-) {
-  return this.usersService.getFollowing(userId, query.cursor, query.limit);
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id/followers')
+  getFollowers(@Param('id') userId: string, @Query() query: FollowersQueryDto) {
+    return this.usersService.getFollowers(userId, query.cursor, query.limit);
+  }
+
+  @Get(':id/following')
+  getFollowing(@Param('id') userId: string, @Query() query: FollowersQueryDto) {
+    return this.usersService.getFollowing(userId, query.cursor, query.limit);
+  }
 }
